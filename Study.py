8e4369625cea04eaea44a85c5e8502d29284edf2
nmurphy101 <nicholas.murphy@mnsu.edu>
2017-04-01 19:45:38 -0500

    Added random block/trial creation, fixed some boolean issues

--- Study.py
+++ Study.py
@@ -1,5 +1,5 @@
 #Package Imports
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 #########################################
@@ -35,12 +35,14 @@ gameSeconds = 0
 # A trial hasn't started yet, so data won't be recorded
 inTrials = False
 # Check for completion of the instructions 
-instructionsComplete = false
+instructionsComplete = False
 # Check for completion of the tutorials and the time of completion
-tutorialsComplete = false
+tutorialsComplete = False
 tutorialsDoneTime = 0
 # Number of trials completed
 trialDoneCounter = 0
+# Number of testing blocks completed (1 block = 12 trials)
+BlocksCompleted = 0
 
 # Trial possibility list that will be used to build random trial stacks
 # Types: size-radius=(Small, Medium, Large), distance-to-center=(close, far),      direction=(Left, Right)
@@ -54,10 +56,20 @@ Le = -1
 Ri = 1
 # Organized list sorted by Size then direction then distance
 PossibleTrialsList = [ [Sm,Cl,Le], [Sm,Fa,Le], [Sm,Cl,Ri], [Sm,Fa,Ri],   [Me,Cl,Le], [Me,Fa,Le], [Me,Cl,Ri], [Me,Fa,Ri],   [La,Cl,Le], [La,Fa,Le], [La,Cl,Ri], [La,Fa,Ri] ]
-
+# Initiliztion of the trial block
+trialBlock = []
 # The random function will give a predictible pattern each run
 random.seed = 50
 
+# Generate the first block of random trials
+for trial in PossibleTrialsList:
+	if randint(1,2) == 1: # Insert the trial at the end of the block if 1
+		trialBlock.append(trial)
+	else: # Insert the trial at the start of the block if 2
+		trialBlock.insert(0,trial)
+		
+# The trail currently being performed
+currentTrial = trialBlock.pop()
 
 # Main game loop
 while True: 
@@ -76,29 +88,29 @@ while True:
 		# Key Press Down Event (ASCII Codes)
 		elif event.type == pygame.KEYDOWN:
 			# If excape key was pressed quit the game
-			if event.key == ord ( "ESC" ): 
+			if event.key == ord( "ESC" ): 
 				pygame.time.set_timer ( pygame.USEREVENT , 0 )
 				pygame.quit()
 				sys.exit()	
 				
 			#Pressing space to complete the instructions and then tutorials to move on to the trials
-			# Key Press Down Event on space
-			if event.key == ord ( "space" );
+			# If space bar is pressed
+			if event.key == ord( "space" ):
 				#Allow the player to complete the instructions 
 				# if they haven't yet and 15 seconds or more have passed
-				if instructionsComplete == false and gameSeconds >= 15:
-					instructionsComplete == true
+				if instructionsComplete == False and gameSeconds >= 15:
+					instructionsComplete == True
 					
 				# Allow the player to complete the tutorial and move to actual trials
 				# if the instructions are complete, the tutorials haven't been yet, and the player has completed 3 or more test trials 
-				if instructionsComplete == true and tutorialComplete == false and trialDoneCounter <=3:
+				if instructionsComplete == True and tutorialComplete == False and trialDoneCounter <=3:
 					#mark the time that the tutorials were completed
 					tutorialsDoneTime = gameSeconds
 					# Zero out trials completed as the ones done thus far were pratice
 					trialDoneCounter = 0
 					tutorialComplete == true
 					inTrials = true
-				
+					##needs more work here
 				
 		# Mouse Press Down Event		
 		elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -115,23 +127,42 @@ while True:
 				# If currently in a trial
 				if inTrials == True:
 					## Increase successful trial counter
-					## 
-					#Remove the code below with actual code
+					# Pop the current test block stack
+					trialBlock.pop()
+					## Generate the next trial stack from current block
+					
+					# If test block is empty generate the next block
+					if not trialBlock:
+						# Generate the next block of random trials
+						for trial in PossibleTrialsList:
+							if randint(1,2) == 1: # Insert the trial at the end of the block if 1
+								trialBlock.append(trial)
+							else: # Insert the trial at the start of the block if 2
+								trialBlock.insert(0,trial)
+					##   and increment the blocksCompleted var
+					
+					##Remove the code below with actual code
 					tempNonsense = 0
 		
 					
 			# Unsuccessful click on the circle object
-			if clickobject.get_rect().collidepoint(x,y):	
+			if clickobject.get_rect().collidepoint(x,y):
 				# If currently in a trial
 				if inTrials == True:
 					## Increase click error count var
-					#Remove the code below with actual code
+					
+					
+					##Remove the place holder code below with actual code
 					tempNonsense = 0
 		
 		# Display section
 		## Display number of trials completed vs needed to do yet (i.e. 2/120)
 		## Code to do this ^ here	
 		
+		if BlocksCompleted == 10:
+			##Do other stuff as well like print
+			pygame.quit()
+			sys.exit()
 	# Draw/Update the screen		
 	pygame.display.update()
 	
